dcd43fbaab9bda4e18ef5886f935b989c8a1e17a
Caio <caioh.maiab@gmail.com>
2025-07-23 18:42:33 -0300

    add webhook test

--- hello_flow.py
+++ hello_flow.py
@@ -7,7 +7,7 @@ def hello_flow(name=""):
     print(f"Hello, {name}, this file has been changed again!")
 
     teams_webhook_block = MicrosoftTeamsWebhook.load("teams-test", validate=False)
-    teams_webhook_block.notify(f"Flow executado para: {name}")
+    teams_webhook_block.notify(f"Flow executado")
 
 if __name__ == "__main__":
 	hello_flow("world")
