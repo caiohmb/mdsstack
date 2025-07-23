d19c40daf11b5ad28b0d948f53a73d2a9c67da1d
Caio <caioh.maiab@gmail.com>
2025-07-23 18:24:01 -0300

    add webhook test

--- hello_flow.py
+++ hello_flow.py
@@ -1,8 +1,13 @@
 from prefect import flow
+from prefect.blocks.notifications import MicrosoftTeamsWebhook
+
 
 @flow(log_prints=True)
 def hello_flow(name=""):
     print(f"Hello, {name}, this file has been changed!")
+
+    teams_webhook_block = MicrosoftTeamsWebhook.load("teams-test")
+    teams_webhook_block.notify(f"Flow executado para: {name}")
 
 if __name__ == "__main__":
 	hello_flow("world")
